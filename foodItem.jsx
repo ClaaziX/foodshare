af5a8d2180b47e782432699fa9a3a675f7be920b
ClaaziX <georgecwnn@hotmail.com>
2016-01-27 10:24:18 +0000

    Update foodItem.jsx

--- foodItem.jsx
+++ foodItem.jsx
@@ -8,17 +8,4 @@ FoodItems = React.createClass({
     FoodItems.remove(this.props.foodItem._id);
   },
  
-  render() {
-    // Give foodItems a different className when they are checked off,
-    // so that we can style them nicely in CSS
-    const foodItemClassName = this.props.foodItem.checked ? "checked" : "";
- 
-    return (
-      <li className={foodItemClassName}>
-        <button className="delete" onClick={this.deleteThisItem}>
-          &times;
-        </button>
-      </li>
-    );
-  }
 });
