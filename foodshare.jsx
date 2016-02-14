9e3edf9af9fbbe993d2e8c5783119a73dc256e9d
thomas nickson <thomas.nickson@gmail.com>
2016-02-14 17:58:46 +0000

    Removed facebook package for now

--- foodshare.jsx
+++ foodshare.jsx
@@ -2,7 +2,13 @@ FoodItemsC = new Mongo.Collection("foodItems");
 
 if (Meteor.isClient) {
   // This code is executed on the client only
- 
+  Accounts.ui.config({
+      
+      passwordSignupFields: "USERNAME_ONLY"
+
+      });
+
+
   Meteor.startup(function () {
     // Use Meteor.startup to render the component after the page is ready
     ReactDOM.render(<App />, document.getElementById("render-target"));
