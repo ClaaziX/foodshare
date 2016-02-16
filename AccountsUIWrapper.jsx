f3fba30ef5fcff91ed052ff0c3eb2a8bcf3a0bb6
thomas nickson <thomas.nickson@gmail.com>
2016-02-16 22:07:09 +0000

    fix AccountsUIWrapper issue

--- AccountsUIWrapper.jsx
+++ AccountsUIWrapper.jsx
@@ -1,5 +1,3 @@
-AccountsUIWrapper = React.createClass({
-<<<<<<< HEAD
   componentDidMount() {
     // Use Meteor Blaze to render login buttons
     this.view = Blaze.render(Template.loginButtons,
@@ -14,34 +12,3 @@ AccountsUIWrapper = React.createClass({
     return <span ref="container" />;
   }
 });
-=======
-
-  componentDidMount() {
-
-    // Use Meteor Blaze to render login buttons
-
-    this.view = Blaze.render(Template.loginButtons,
-
-      React.findDOMNode(this.refs.container));
-
-  },
-
-  componentWillUnmount() {
-
-    // Clean up Blaze view
-
-    Blaze.remove(this.view);
-
-  },
-
-  render() {
-
-    // Just render a placeholder container that will be filled in
-
-    return <span ref="container" />;
-
-  }
-
-});
-
->>>>>>> origin/master
