5364466500e79b47fcdde23e75197b4504bdcd69
ClaaziX <georgecwnn@hotmail.com>
2016-02-24 16:54:39 +0000

    Added files via upload

--- FoodItems.jsx
+++ FoodItems.jsx
@@ -8,6 +8,16 @@ FoodItems = React.createClass({
     FoodItemsC.remove(this.props.foodItem._id);
   },
 
+  claimThisItem() {
+    var pCla = this.props.foodItem.portionsClaimed;
+    var pNum = this.props.foodItem.portionNo - pCla;
+    var itemId = this.props.foodItem._id;
+    if (pNum > 0) {
+      pCla++;
+      FoodItemsC.update(itemId, {$set: {portionsClaimed: pCla} });
+    }else{ alert("Sorry! No more portions left!"); }
+  },
+
 genPrtnImg: function () {
   var pCla = this.props.foodItem.portionsClaimed;
   var pNum = this.props.foodItem.portionNo - pCla;
@@ -29,7 +39,7 @@ genPrtnImg: function () {
           <td rowSpan="3"><img className="itemSmlPic" src="http://bed56888308e93972c04-0dfc23b7b97881dee012a129d9518bae.r34.cf1.rackcdn.com/sites/default/files/veggie-heart.jpg"></img></td>
           <td><h1>{this.props.foodItem.foodName}</h1>
           { Meteor.user().username == this.props.foodItem.username ?
-            <button className="delete" onClick={this.deleteThisItem}>x</button> : ''
+            <button className="delete" onClick={this.deleteThisItem}>x</button> : <button className="claim" onClick={this.claimThisItem}>Claim</button>
           }
           </td>
           <td rowSpan="2"><img className="profilePic" src="http://thesocialmediamonthly.com/wp-content/uploads/2015/08/photo.png"></img></td>
