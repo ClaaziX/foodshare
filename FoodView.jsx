084fbbd0a6e4e34d82a57e1cdf5f909a6c2ea06d
thomas nickson <thomas.nickson@gmail.com>
2016-02-25 20:51:52 +0400

    moved the code for item creation in to its own component but it probably wants to be its own page

--- FoodView.jsx
+++ FoodView.jsx
@@ -6,8 +6,6 @@ FoodView = React.createClass({
   getInitialState() {
     return {
       filter : '',
-      files: [],
-      arrayOfImageIds: [],
     }
   },
 
@@ -21,32 +19,6 @@ FoodView = React.createClass({
       currentUser: Meteor.user()
     };
   },
-  
-
-  onDrop: function (files) {
-    if (Meteor.isServer) {
-    MyImages.insert(files, function(err, fileObj){
-        if(err){
-          alert("Error");
-        } else {
-          // gets the ID of the image that was uploaded
-          var imageId = fileObj._id;
-          // do something with this image ID, like save it somewhere
-          arrayOfImageIds.push(imageId);
-        }
-       }); 
-    }  
-  
-
-    this.setState({
-      files: files
-    });
-  },
-
-  onOpenClick: function () {
-    this.refs.dropzone.open();
-  },
-
 
   filterList(event) {
     //Change the state of the filtering in play
@@ -62,70 +34,13 @@ FoodView = React.createClass({
     });
   },
 
-    handleSubmit(event) {
-    event.preventDefault();
- 
-    // Find the text field via the React ref
-    var foodName = ReactDOM.findDOMNode(this.refs.FNR).value.trim();
-    var	foodDesc = ReactDOM.findDOMNode(this.refs.FDR).value.trim();
-    var portionSelect = ReactDOM.findDOMNode(this.refs.PSR).value.trim();
-    // var imgURL = arrayOfImageIds[0];
-    
-    console.log('BEGIN')
-    console.log(portionSelect)
-    console.log('END')
 
-
-    FoodItemsC.insert({
-      foodName: foodName,
-      foodDesc: foodDesc,
-      portionNo: portionSelect,
-      portionsClaimed: 0,
-      // imgURL: imgURL,
-      owner: Meteor.userId(),           // _id of logged in user
-      username: Meteor.user().username,  // username of logged in user
-      createdAt: new Date() // current time
-    });
- 
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.FNR).value = "";
-    ReactDOM.findDOMNode(this.refs.FDR).value = "";
-    ReactDOM.findDOMNode(this.refs.PSR).value = "";
-
-  },
-
-genPtNo: function (pNo) {
-  var x = [];
-  for (i = 1; i < pNo; i++){
-    x.push(<option value={i}>{i}</option>);
-  }
-  return <select name="portionSelect" ref="PSR">{x}</select>;
-},
 
   render: function() {
     return (
 	<div>
           { this.data.currentUser ?
-            <form className="new-foodItem" onSubmit={this.handleSubmit}>
-	            
-              <input type="text" name="foodName" ref="FNR" placeholder="Please enter the name of the food" /><br />
-	            
-              <div>
-                
-                {this.state.files.length > 0 ? <div>
-                  <p>Uploading {this.state.files.length} picture...</p>
-                  <div>{this.state.files.map((file) => <img className="imgPreview" src={file.preview} /> )}</div>
-                  </div> : <Dropzone ref="dropzone" onDrop={this.onDrop}>
-                  <div>Try dropping some files here, or click to select files to upload.</div>
-                </Dropzone>}
-              </div>
-              
-              <input type="text" name="foodDesc" ref="FDR" placeholder="Please enter a description of the food" /><br />
-              
-              <div>Please Select The Number of Portions:</div>
-              {this.genPtNo(20)}
-              <input type="submit" id="submit" />
-            </form> : ''
+         <ItemCreation />: ''
           }
 
 	<input type="text" placeholder="Search" onChange={this.filterList}/>
--- ItemCreation.jsx
+++ ItemCreation.jsx
@@ -1,5 +1,99 @@
 ItemCreation = React.createClass({
+  
+  getInitialState(){
+    return{
+      files: [],
+      arrayOfImageIds: [],
+    }
+  },
+
+  onDrop: function (files) {
+    if (Meteor.isServer) {
+    MyImages.insert(files, function(err, fileObj){
+        if(err){
+          alert("Error");
+        } else {
+          // gets the ID of the image that was uploaded
+          var imageId = fileObj._id;
+          // do something with this image ID, like save it somewhere
+          arrayOfImageIds.push(imageId);
+        }
+       }); 
+    }  
+  
+
+    this.setState({
+      files: files
+    });
+  },
+
+  onOpenClick: function () {
+    this.refs.dropzone.open();
+  },
+
+genPtNo: function (pNo) {
+  var x = [];
+  for (i = 1; i < pNo; i++){
+    x.push(<option value={i}>{i}</option>);
+  }
+  return <select name="portionSelect" ref="PSR">{x}</select>;
+},
+
+    handleSubmit(event) {
+    event.preventDefault();
+ 
+    // Find the text field via the React ref
+    var foodName = ReactDOM.findDOMNode(this.refs.FNR).value.trim();
+    var	foodDesc = ReactDOM.findDOMNode(this.refs.FDR).value.trim();
+    var portionSelect = ReactDOM.findDOMNode(this.refs.PSR).value.trim();
+    // var imgURL = arrayOfImageIds[0];
+    
+    console.log('BEGIN')
+    console.log(portionSelect)
+    console.log('END')
+
+
+    FoodItemsC.insert({
+      foodName: foodName,
+      foodDesc: foodDesc,
+      portionNo: portionSelect,
+      portionsClaimed: 0,
+      // imgURL: imgURL,
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username,  // username of logged in user
+      createdAt: new Date() // current time
+    });
+ 
+    // Clear form
+    ReactDOM.findDOMNode(this.refs.FNR).value = "";
+    ReactDOM.findDOMNode(this.refs.FDR).value = "";
+    ReactDOM.findDOMNode(this.refs.PSR).value = "";
+
+  },
+
+
 	render() {
-		 return ('');
+		 return (
+		 	    <form className="new-foodItem" onSubmit={this.handleSubmit}>
+	            
+              <input type="text" name="foodName" ref="FNR" placeholder="Please enter the name of the food" /><br />
+	            
+              <div>
+                
+                {this.state.files.length > 0 ? <div>
+                  <p>Uploading {this.state.files.length} picture...</p>
+                  <div>{this.state.files.map((file) => <img className="imgPreview" src={file.preview} /> )}</div>
+                  </div> : <Dropzone ref="dropzone" onDrop={this.onDrop}>
+                  <div>Try dropping some files here, or click to select files to upload.</div>
+                </Dropzone>}
+              </div>
+              
+              <input type="text" name="foodDesc" ref="FDR" placeholder="Please enter a description of the food" /><br />
+              
+              <div>Please Select The Number of Portions:</div>
+              {this.genPtNo(20)}
+              <input type="submit" id="submit" />
+            </form>
+		 );
 	}
 });
