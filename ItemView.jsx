56dcc58f516b981db8466bebc60b1d8853685d55
thomas nickson <thomas.nickson@gmail.com>
2016-03-01 18:27:19 +0400

    comments insert in the itemview

--- ItemView.jsx
+++ ItemView.jsx
@@ -9,12 +9,31 @@ ItemView = React.createClass({
 	
 	},
 	 
+	addComment(event) {
+		 event.preventDefault()   
+            	 var comment = ReactDOM.findDOMNode(this.refs.comment).value.trim();
+
+		 FoodItemsC.update({_id: this.data.foodItem._id},{$push : {
+		 	comment:{
+			username: Meteor.user().username,
+			comment: comment,
+			createdAt: new Date()
+			}
+		 }});
+
+		 ReactDOM.findDOMNode(this.refs.comment).value = "";
+
+	},
+
 	render() {
 		 return (
 		 <div>
 			<table className="itemListView">
 		 	 	<FoodItems key={this.data.foodItem._id} foodItem={this.data.foodItem}/>
 			</table>
+			<form className="new-comment" onSubmit={this.addComment}>
+			      Comment: <input type="text" name="comment" ref="comment" />
+			</form>
 		</div>
 			);
 	}
