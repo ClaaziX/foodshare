5bb9b686be495757dfcdcf60802149ad7f50e771
ClaaziX <georgecwnn@hotmail.com>
2016-03-01 13:52:26 +0000

    Material UI - Small working example

--- AppHeader.jsx
+++ AppHeader.jsx
@@ -1,5 +1,49 @@
+var {
+    AppCanvas,
+    AppBar,
+    Styles,
+    RaisedButton,
+    IconMenu,
+    IconButton,
+    MenuItem,
+    MoreVertIcon,
+    TextField,
+    NavigationClose,
+    Avatar,
+    DatePicker,
+    Tabs,
+    Tab,
+    Slider,
+    FlatButton,
+    FontIcon
+    } = MUI;
+   
+var { ThemeManager, LightRawTheme } = Styles;
+
+const styles = {
+	exampleImageInput: {
+		cursor: 'pointer',
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		right: 0,
+		left: 0,
+		width: '100%',
+		opacity: 0,
+	},
+};
+
 AppHeader = React.createClass({
 
+    childContextTypes: {
+        muiTheme: React.PropTypes.object
+    },
+ 
+    getChildContext() {
+        return {
+            muiTheme: ThemeManager.getMuiTheme(LightRawTheme)
+        };
+    },
 
     render : function(){
 	    return(
@@ -10,7 +54,27 @@ AppHeader = React.createClass({
 			<AccountsUIWrapper />
 		    </header>
 
-		    {this.props.children}    
+		    {this.props.children} 
+
+		    <div className="tabsContain">
+				<FlatButton label="List"
+					labelPosition="before"
+					primary={true}
+					style={styles.button}
+				/>
+
+				<FlatButton label="Map"
+					labelPosition="before"
+					primary={true}
+					style={styles.button}
+				/>
+
+				<FlatButton label="Messages"
+					labelPosition="before"
+					primary={true}
+					style={styles.button}
+				/>
+			</div>
 
 		</div>    
 	    );
