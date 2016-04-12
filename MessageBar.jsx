428d72687683ce8d38f7d65f2db5525a2f669ba4
thomas nickson <thomas.nickson@gmail.com>
2016-04-12 22:19:22 +0100

    side bar shows messages

--- MessageBar.jsx
+++ MessageBar.jsx
@@ -6,50 +6,57 @@ var {
 } = MUI;
 
 var {SvgIcons}=MUI.Libs;
+
 MessageBar = React.createClass({
 
-	   mixins: [ReactMeteorData],
+    mixins: [ReactMeteorData],
 
-	   renderMessagesList: function(){
-	   		       return(
-	   		       <ListItem	
-			      		leftAvatar={<Avatar src="http://thesocialmediamonthly.com/wp-content/uploads/2015/08/photo.png"/>}
-					rightIconButton={<SvgIcons.CommunicationChatBubble />}
-					primaryText="Steven Username"
-					secondaryText={
-						<p>
-							<span style={{color: Styles.Colors.darkBlack}}>The Date</span><br/>
-							Little bit of the text from the messages
-						</p>
-						}
-					secondaryTextLines={2}
+    renderMessagesList: function(){
+	if(this.data.privateMessages){
+	    return this.data.privateMessages.messages.map((message) => {
 
-			      />
-	   		      );		       
+		return(
+		    <ListItem	
+                        leftAvatar={<Avatar src="http://thesocialmediamonthly.com/wp-content/uploads/2015/08/photo.png"/>}
+			rightIconButton={<SvgIcons.CommunicationChatBubble />}
+			primaryText={message.username}
+			secondaryText={
+			    <p>
+				<span style={{color: Styles.Colors.darkBlack}}>The Date</span><br/>
+					    Little bit of the text from the messages
+			    </p>
+				      }
+			secondaryTextLines={2}
 
-	   		       },
+		    />
+		)
+	    });
+	}
 
-	   getMeteorData: function(){
-	   		  //[0].messages needs to be added to messages if it exists
-	   		  currentUser = Meteor.user() ? Meteor.user().username : '';
-			  return {
-			  	    currentUser: currentUser,
-				    messages: PrivateChatC.find({between: {$exists: true, $all: [currentUser]}}
-				    	      	               ).fetch()[0]
-		          };
+    },
+
+    getMeteorData: function(){
+
+	currentUser = Meteor.user() ? Meteor.user().username : '';
+	return {
+	    currentUser: currentUser,
+	    privateMessages: PrivateChatC.find(
+		{between: {$exists: true, $all: [currentUser]}}
+	    ).fetch()[0]
+	};
 
 
-		},
+    },
 
 
-	   render : function(){
-	   	  console.log(this.data.messages?'true':'false');
-	   	  return(
-			<List subheader="Messages">
-			      {this.renderMessagesList()}
-			</List>
-		  );
+    render : function(){
+	console.log(this.data.privateMessages);
+	return(
+	    <List subheader="Messages">
+		{this.renderMessagesList()}
+	    </List>
+	);
 
-	   }
+    }
 
 })
