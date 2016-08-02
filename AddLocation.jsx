1430cfa7c0ba2ec74a254fecd852404cbcc150bd
thomas nickson <thomas.nickson@gmail.com>
2016-08-02 18:48:57 +0400

    maps and geosuggest work together

--- AddLocation.jsx
+++ AddLocation.jsx
@@ -2,23 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import GoogleMapAdd from './GoogleMapAdd.jsx';
-import GeoComplete from './GeoComplete.jsx'
-import { Meteor } from 'meteor/meteor'
+import GeoComplete from './GeoComplete.jsx';
+import Geosuggest from 'react-geosuggest';
+import { Meteor } from 'meteor/meteor';
 
 const AddLocation = React.createClass({
+
     getInitialState(){
-	return({loaded:false})
+	return({loaded:false});
 	},
+
+    getCoordsMap(coords){
+	this.setState({coords:coords});
+    },
+
+    getCoordsGeosuggest(suggest){
+
+	var coords = suggest.location;
+	this.setState({coords:coords,marker:coords});
+
+
+    },
+
     loaded(loaded){
 	this.setState({loaded:loaded});
-	console.log(this.state.loaded)
+
 	},
+
     render() {
 	return (
 	    <div>
 		{this.state.loaded ?
-		<GeoComplete/> : ''}
-  		<ALMapView loaded={this.loaded}/>
+		<Geosuggest onSuggestSelect={this.getCoordsGeosuggest}/> : ''}
+  		<ALMapView loaded={this.loaded} getCoords={this.getCoordsMap} suggestionMarker={this.state.marker}/>
 	    </div>
 	);
     }     
@@ -31,20 +47,36 @@ export default AddLocation;
 const ALMapView = React.createClass({
 
     mixins: [ReactMeteorData],
-
+    
     listeners(map) {
+
+        //Let other elements know that the map is loaded and ready
+    	this.props.loaded(true);
+	
+	//set the map instance to state
+	this.setState({map:map.instance});	
+
+	//Function to set the coords 
 	var getCoords = (function(marker){
 	    this.setState({location:marker.position});
 	}).bind(this);
 
-	this.props.loaded(true);
-	console.log(this.props);
-	this.setState({map:map.instance});
+	//Function to set the marker 
+	var setMarker = (function(marker){
+	    this.setState({marker:marker});
+	}).bind(this);
+
+	//Function to get the marker
+	var getMarker = (function(){
+	    return this.state.marker;
+	}).bind(this);
 	
 
-	var Marker;
-
 	map.instance.addListener('click', function(e){
+
+	    //Get marker ref
+	    var Marker = getMarker();
+	    
 	    if(Marker != undefined){
 		Marker = Marker.setMap(null);
 		}
@@ -52,16 +84,22 @@ const ALMapView = React.createClass({
 		position:e.latLng,
 		map:map.instance
 	    });
-
+	    
+	    //set the marker ref
+	    setMarker(Marker);
+	    
+	    //Set coords in state
 	    getCoords(Marker);
 		
 	})
     },
 
     componentDidMount() {
-  	GoogleMaps.load();
+  	GoogleMaps.load({key:'AIzaSyC_muGOORl5WqYTyBMJtEw05AESKYBm3nY',libraries:'places'});
+
     },
     getMeteorData() {
+
 	return {
 	    loaded: GoogleMaps.loaded(),
 	    mapOptions: GoogleMaps.loaded() && this._mapOptions()
@@ -74,8 +112,39 @@ const ALMapView = React.createClass({
 	};
     },
 
+    componentWillReceiveProps(nextProps){
+
+    if (nextProps.suggestionMarker!=undefined){
+	this.setMarkerFromProp(nextProps);
+	};
+
+    },
+
+    setMarkerFromProp(nextProps){
+	var map = this.state.map;
+	var Marker = this.state.marker;
+
+
+
+
+	if(Marker != undefined){
+		Marker = Marker.setMap(null);
+		}
+
+	Marker = new google.maps.Marker({
+	    position:new google.maps.LatLng(nextProps.suggestionMarker),
+	    map:map
+	    });
+
+	this.setState({marker:Marker});
+
+	return(Marker)
+	
+	
+    },
 
     render() {
+
 
 
 	if (this.data.loaded)
