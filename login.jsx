b120512296b4393e7e3240047e40b4e901b90059
thomas nickson <thomas.nickson@gmail.com>
2016-08-12 17:36:58 +0400

    fix the login not rendering bug

--- login.jsx
+++ login.jsx
@@ -5,12 +5,17 @@ import Accounts from 'meteor/std:accounts-basic';
 
 const login = React.createClass({ 
 
+      shouldComponentUpdate(nextProps, nextState){
+      				       return false;
+      },
+
 	handleLogin : function () {
 		this.router.push('/');
 	},
 
 	render : function () {
 	  	return(
+		
 		    <div>
 		    	<Accounts.ui.LoginForm redirect={this.handleLogin} />
 		    </div>
