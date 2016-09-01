6b2fdf61534fad332b3e7898397529cbd49189f7
thomas nickson <thomas.nickson@gmail.com>
2016-09-01 16:49:36 +0400

    fixed slight bug in itemview

--- ItemCreation.jsx
+++ ItemCreation.jsx
@@ -51,13 +51,13 @@ const paperStyle = {
 
 const styles = {
     headline: {
-    fontSize: 24,
-    paddingTop: 16,
-    marginBottom: 12,
-    fontWeight: 400,
+        fontSize: 24,
+        paddingTop: 16,
+        marginBottom: 12,
+        fontWeight: 400,
     },
     slide: {
-    padding: 10,
+        padding: 10,
     },
 };
 
@@ -68,162 +68,164 @@ const ItemCreation = React.createClass({
     getMeteorData(){
 
         return{	
-		
-			addedItems : FoodItemsC.find({'imgURL':this.state.imageURL}).fetch()
-			    }
+	    
+	    addedItems : FoodItemsC.find({'imgURL':this.state.imageURL}).fetch()
+	}
     },
 
     getInitialState(){
-    return{
-        //State for the stepper
+        return{
+            //State for the stepper
 	    finished: false,
-	        stepIndex: 0,
-		    completedIndex: 0,
-		        
-			    //Once the image is uploaded this gets set with the query for the items
-			        imageURL:'{not:/*/}',
+	    stepIndex: 0,
+	    completedIndex: 0,
+	    
+	    //Once the image is uploaded this gets set with the query for the items
+	    imageURL:'{not:/*/}',
 
-				    //For the snackbar
-				        open:false,
-					}
+	    //For the snackbar
+	    open:false,
+	}
     },
 
     //Snackbar code
 
     handleError() {
-    this.setState({
-        open: true,
+        this.setState({
+            open: true,
 	});
     },
 
     handleRequestClose() {
-    this.setState({
-        open: false,
+        this.setState({
+            open: false,
 	});
     },
 
     //Stepper Code 
     handleNext() {
 
-    stepIndex = this.state.stepIndex;
-    if((stepIndex+1) == this.state.completedIndex){
-        this.setState({
+        stepIndex = this.state.stepIndex;
+        if((stepIndex+1) == this.state.completedIndex){
+            this.setState({
 		stepIndex: stepIndex + 1,
-			   finished: stepIndex >= 2,
-			       });
-			       } else {
-			           this.handleError()
-				   }
+		finished: stepIndex >= 2,
+	    });
+	} else {
+	    this.handleError()
+	}
     },
 
     handlePrev(){
-    stepIndex = this.state.stepIndex;
-    if (stepIndex > 0){
-        this.setState({stepIndex: stepIndex - 1});
+        stepIndex = this.state.stepIndex;
+        if (stepIndex > 0){
+            this.setState({stepIndex: stepIndex - 1});
 	}
     },
 
     onUpload(url){
 
-    this.setState({imageURL:url,
-	       completedIndex:1})
+        this.setState({imageURL:url,
+	               completedIndex:1})
 
     },
 
     onCoordSelection(location){
-    this.setState({latLng:{lat:location.latLng.lat(),lng:location.latLng.lng()},
-	       address:location.address,
+        this.setState({latLng:{lat:location.latLng.lat(),lng:location.latLng.lng()},
+	               address:location.address,
 		       completedIndex:2
-		       })
+	})
 
     },
 
     handleSubmit(item){
-    
-    FoodItemsC.insert({
-        foodName: item.name,
-	        portionNo: item.portions,
-		    portionsClaimed: 0,
-		        imgURL: this.state.imageURL,
-			    location:this.state.latLng,
-			        address:this.state.address,
-				    owner: Meteor.userId(),           // _id of logged in user
-				        username: Meteor.user().username,  // username of logged in user
-					    createdAt: new Date() // current time
-					    });
-					    this.setState({completedIndex:3})
-					        
+        
+        FoodItemsC.insert({
+            foodName: item.name,
+	    portionNo: item.portions,
+	    portionsClaimed: 0,
+	    imgURL: this.state.imageURL,
+	    location:this.state.latLng,
+	    address:this.state.address,
+	    owner: Meteor.userId(),           // _id of logged in user
+	    username: Meteor.user().username,  // username of logged in user
+	    createdAt: new Date() // current time
+	});
+	this.setState({completedIndex:3})
+	
     },
     
     genStepButtons(step) {
 
-    const {stepIndex} = this.state;
+        const {stepIndex} = this.state;
 
         return (
-	       <div style={{margin: '12px 0'}}>
-	       	        <RaisedButton
-					label={stepIndex === 2 ? 'Finish' : 'Next'}
-							     disableTouchRipple={true}
-									disableFocusRipple={true}
-											primary={true}
-													onTouchTap={stepIndex === 3 ? browserHistory.push('/')  : this.handleNext}
-															      	  style={{marginRight: 12}}
-																  		           />
-																			        {step > 0 && (
-																				      	 <FlatButton
-																					        label="Back"
-																						       disabled={stepIndex === 0}
-																						       			            disableTouchRipple={true}
-																										           disableFocusRipple={true}
-																											          onTouchTap={this.handlePrev}
-																												     />
-																												           )}
-																													    </div>
-																													        );
+	    <div style={{margin: '12px 0'}}>
+	       	<RaisedButton
+label={stepIndex === 2 ? 'Finish' : 'Next'}
+		    disableTouchRipple={true}
+disableFocusRipple={true}
+primary={true}
+onTouchTap={stepIndex === 3 ? browserHistory.push('/')  : this.handleNext}
+		    style={{marginRight: 12}}
+		/>
+		{step > 0 && (
+		     <FlatButton
+			 label="Back"
+			 disabled={stepIndex === 0}
+			 disableTouchRipple={true}
+			 disableFocusRipple={true}
+			 onTouchTap={this.handlePrev}
+		     />
+		 )}
+	    </div>
+	);
     },
 
     render() {
-    const {finished, stepIndex} = this.state;
-    const contentStyle = {margin: '0 16px'};
+        const {finished, stepIndex} = this.state;
+        const contentStyle = {margin: '0 16px'};
 
-    return (
-        <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
-	     <Stepper activeStep={stepIndex} orientation="vertical">
-	     	          <Step>
-					<StepLabel>Upload a Photograph of the Item(s)</StepLabel>
-							    <StepContent>
-									    <PhotoUpload onUpload={this.onUpload}/>
-												    {this.genStepButtons(0)}
-														</StepContent>
-															    </Step>
-															         <Step>
-																   <StepLabel>Add the Location of the Item(s)</StepLabel>
-																   		      <StepContent>
-																		            <AddLocation onCoordSelection={this.onCoordSelection}/>
-																			    		      {this.genStepButtons(1)}
-																					        </StepContent>
-																						     </Step>
-																						          <Step>
-																							    <StepLabel>Add Details of the Item(s)</StepLabel>
-																							    		   	   <StepContent>
-																										         <AddItem handleSubmit = {this.handleSubmit}/>
-																											 	  	           <br/>
-																														         <FoodView renderer="list" foodItems={this.data.addedItems}/>
+        return (
+            <div style={{width: '100%', maxWidth: 700, margin: 'auto'}}>
+	        <Stepper activeStep={stepIndex} orientation="vertical">
+	     	    <Step>
+			<StepLabel>Upload a Photograph of the Item(s)</StepLabel>
+			<StepContent>
+			    <PhotoUpload onUpload={this.onUpload}/>
+			    {this.genStepButtons(0)}
+			</StepContent>
+		    </Step>
+		    <Step>
+			<StepLabel>Add the Location of the Item(s)</StepLabel>
+			<StepContent>
+			    <AddLocation onCoordSelection={this.onCoordSelection}/>
+			    {this.genStepButtons(1)}
+			</StepContent>
+		    </Step>
+		    <Step>
+			<StepLabel>Please list all of your items pressing submit after each one until you have added everything in your picture.</StepLabel>
+			<StepContent>
+                            <br/>
+			    <AddItem handleSubmit = {this.handleSubmit}/>
+			    <br/>
 
-																															 	   		       {this.genStepButtons(2)}
-																																		         </StepContent>
-																																			      </Step>
-																																			       </Stepper>
+			    <FoodView renderer="list" foodItems={this.data.addedItems} imageURL=''/>
 
-																																			        <Snackbar
-																																				     open={this.state.open}
-																																				          message="Please complete this section before moving on."
-																																					  		      autoHideDuration={4000}
-																																							           onRequestClose={this.handleRequestClose}
-																																								    />
-																																								        </div>
-																																									);
-																																									
+			    {this.genStepButtons(2)}
+			</StepContent>
+		    </Step>
+		</Stepper>
+
+		<Snackbar
+		    open={this.state.open}
+		    message="Please complete this section before moving on."
+		    autoHideDuration={4000}
+		    onRequestClose={this.handleRequestClose}
+		/>
+	    </div>
+	);
+	
     }
 });
 
--- foodshare.jsx
+++ foodshare.jsx
@@ -145,72 +145,72 @@ if (Meteor.isServer) {
     });
     
     //Reset all the databases before we add the test data in.
-
-    //Remove Users
-    Meteor.users.remove({});
-
-    //Remove Fooditems
-    FoodItemsC.remove({});
-
-    //Remove privateChat
-    PrivateChatC.remove({});
-
-    //Create user accounts
-    var numUsers = 5;
-    for(i = 0; i < numUsers; i++){
-    	  Accounts.createUser({username:'tom'+i,email:'tom'+i+'@mail.com', password:'password'});
-    }
-
-    //Create food items by random users
-    images = ["http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/istock_000050960496_medium.jpg","http://ichef.bbci.co.uk/news/660/cpsprodpb/1325A/production/_88762487_junk_food.jpg","http://www.foodmanufacture.co.uk/var/plain_site/storage/images/publications/food-beverage-nutrition/foodmanufacture.co.uk/npd/top-10-functional-food-trends/11097085-1-eng-GB/Top-10-functional-food-trends_strict_xxl.jpg"]
-    var numFoodItems = 20;
-    var numPortions = 10;
-    for(i = 0; i < numFoodItems; i++){
-        currUser = Meteor.users.find().fetch()[Math.floor(Math.random()*numUsers)]
-    	FoodItemsC.insert({
-     		foodName: faker.lorem.words(),
-      		foodDesc: faker.lorem.sentence(),
-      		portionNo: Math.floor(Math.random()*numPortions),
-      		portionsClaimed: 0,
-      		imgURL: images[i % 3],
-      		owner: currUser._id,
-      		username: currUser.username,
-      		createdAt: new Date(),
-		location: {lat:faker.address.latitude(), lng:faker.address.longitude()}
-      	});
-    	  
-    }
-
-    //Create some claims
-    var numClaims = 30;
-    
-
-    //Create comments by random users
-    var numComments = 35;
-    for(i = 0; i < numComments; i++){
-          currUser = Meteor.users.find().fetch()[Math.floor(Math.random()*numUsers)];
-    	  currItem = FoodItemsC.find().fetch()[Math.floor(Math.random()*numFoodItems)];
-              Meteor.call('createClaims', currUser.username,Math.floor(Math.random()*currItem.portionNo),currItem._id )
-	      FoodItemsC.update({_id: currItem._id},{$push : {
-	      	comments:{
-			username: currUser.username,
-	    		comment: faker.lorem.sentences(),
-	    		createdAt: new Date()
-			}}});
-  
-     }    
-    
-    //Create private messages between people
-    var numMessages = 100;
-    for(i = 0; i < numComments; i++){
-    	  randNumFirst = Math.floor(Math.random()*numUsers);
-	  randNumSecond = (randNumFirst + ((Math.floor(Math.random()*(numUsers-1)))+1) ) % numUsers;
-          allUsers = Meteor.users.find().fetch();
-	  firstUser = allUsers[randNumFirst].username;
-	  secondUser = allUsers[randNumSecond].username;
-          Meteor.call('addPrivateMessage',[firstUser,secondUser],firstUser,faker.lorem.sentences());
-     }    
-    
+    /* 
+     *     //Remove Users
+     *     Meteor.users.remove({});
+     * 
+     *     //Remove Fooditems
+     *     FoodItemsC.remove({});
+     * 
+     *     //Remove privateChat
+     *     PrivateChatC.remove({});
+     * 
+     *     //Create user accounts
+     *     var numUsers = 5;
+     *     for(i = 0; i < numUsers; i++){
+     *     	  Accounts.createUser({username:'tom'+i,email:'tom'+i+'@mail.com', password:'password'});
+     *     }
+     * 
+     *     //Create food items by random users
+     *     images = ["http://images.mentalfloss.com/sites/default/files/styles/article_640x430/public/istock_000050960496_medium.jpg","http://ichef.bbci.co.uk/news/660/cpsprodpb/1325A/production/_88762487_junk_food.jpg","http://www.foodmanufacture.co.uk/var/plain_site/storage/images/publications/food-beverage-nutrition/foodmanufacture.co.uk/npd/top-10-functional-food-trends/11097085-1-eng-GB/Top-10-functional-food-trends_strict_xxl.jpg"]
+     *     var numFoodItems = 20;
+     *     var numPortions = 10;
+     *     for(i = 0; i < numFoodItems; i++){
+     *         currUser = Meteor.users.find().fetch()[Math.floor(Math.random()*numUsers)]
+     *     	FoodItemsC.insert({
+     *      		foodName: faker.lorem.words(),
+     *       		foodDesc: faker.lorem.sentence(),
+     *       		portionNo: Math.floor(Math.random()*numPortions),
+     *       		portionsClaimed: 0,
+     *       		imgURL: images[i % 3],
+     *       		owner: currUser._id,
+     *       		username: currUser.username,
+     *       		createdAt: new Date(),
+     * 		location: {lat:faker.address.latitude(), lng:faker.address.longitude()}
+     *       	});
+     *     	  
+     *     }
+     * 
+     *     //Create some claims
+     *     var numClaims = 30;
+     *     
+     * 
+     *     //Create comments by random users
+     *     var numComments = 35;
+     *     for(i = 0; i < numComments; i++){
+     *           currUser = Meteor.users.find().fetch()[Math.floor(Math.random()*numUsers)];
+     *     	  currItem = FoodItemsC.find().fetch()[Math.floor(Math.random()*numFoodItems)];
+     *               Meteor.call('createClaims', currUser.username,Math.floor(Math.random()*currItem.portionNo),currItem._id )
+     * 	      FoodItemsC.update({_id: currItem._id},{$push : {
+     * 	      	comments:{
+     * 			username: currUser.username,
+     * 	    		comment: faker.lorem.sentences(),
+     * 	    		createdAt: new Date()
+     * 			}}});
+     *   
+     *      }    
+     *     
+     *     //Create private messages between people
+     *     var numMessages = 100;
+     *     for(i = 0; i < numComments; i++){
+     *     	  randNumFirst = Math.floor(Math.random()*numUsers);
+     * 	  randNumSecond = (randNumFirst + ((Math.floor(Math.random()*(numUsers-1)))+1) ) % numUsers;
+     *           allUsers = Meteor.users.find().fetch();
+     * 	  firstUser = allUsers[randNumFirst].username;
+     * 	  secondUser = allUsers[randNumSecond].username;
+     *           Meteor.call('addPrivateMessage',[firstUser,secondUser],firstUser,faker.lorem.sentences());
+     *      }    */
+    //Publish the aggregate sidebar here
 	Meteor.publish("sidebar", function(username){
 		ReactiveAggregate(this, PrivateChatC,
 			[
